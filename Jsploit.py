3c14e0a1d1ed4e5135f2917bf5614c87747b39a9
Japple <jayhyonhan@gmail.com>
2024-04-08 21:18:36 +0100

    Finished the module system. Now need to integrate actual exploits into this program

--- Jsploit.py
+++ Jsploit.py
@@ -7,7 +7,7 @@ class mod_option():
         self.opt = optional
 
     def __str__(self) -> str:
-        return (f"{self.name}    {self.desc}  (required: {self.opt})")
+        return (f"{self.name:<30}{self.desc:<50}  (required: {self.opt})")
 
 modules = { # name: [path, description, options]
             "exploit":{
@@ -24,7 +24,7 @@ modules = { # name: [path, description, options]
         }
 
 search_results = {} # variable to store search results
-search_results_use = {}
+current_module = {}
 
 def f_search(keyword, i_dict: dict): # function to search a nested dictionary
     global search_results
@@ -36,14 +36,13 @@ def f_search(keyword, i_dict: dict): # function to search a nested dictionary
                 search_results[i[0]] = i[1]
 
 def f_search_use(keyword, i_dict: dict): # function to search a nested dictionary (used by use command)
-    global search_results_use
+    global current_module
     for i in i_dict.items():
         if type(i[1]) == dict:
             f_search_use(keyword, i[1])
         else:
             if keyword in i[0] or keyword in i[1][0] or keyword in i[1][1]:
-                search_results_use[i[0]] = i[1]
-            
+                current_module[i[0]] = i[1]
 
 class CLI(cmd.Cmd):
     prompt = "Jsploit >> "
@@ -70,7 +69,19 @@ J\\_____/JJ\\______/FJ  _____/LJ__LJ\\______/FJ__L\\_____/
         'Exits the program'
         print("\nBye")
         exit()
-    
+
+    def do_banner(self, line):
+        'Prints the banner'
+        print("""
+     _                        __            __   _    
+     L]     ____     _ ___    LJ    ____    LJ  FJ_   
+     | L   F ___J   J '__ J   FJ   F __ J      J  _|  
+     | |  | '----_  | |--| | J  L | |--| |  FJ | |-'  
+.--__J J  )-____  L F L__J J J  L F L__J J J  LF |__-.
+J\\_____/JJ\\______/FJ  _____/LJ__LJ\\______/FJ__L\\_____/
+ J_____/  J______F |_J_____F |__| J______F |__|J_____F
+                                             L_J   """)
+
     def do_clear(self, line): # clear the screen
         'Clears the screen'
         global search_results
@@ -84,6 +95,9 @@ J\\_____/JJ\\______/FJ  _____/LJ__LJ\\______/FJ__L\\_____/
         keys = []
         temp = {}
         f_search(keyword, modules)
+        if search_results == {}:
+            print("No results found.")
+            return
         for k,v in search_results.items(): keys.append(k)
         keys = sorted(keys) # sort alphabetical order
         for k in keys: temp[k] = search_results[k]
@@ -181,33 +195,39 @@ J\\_____/JJ\\______/FJ  _____/LJ__LJ\\______/FJ__L\\_____/
                         i += 1
                         print("  {num:<9}{name:30}{path:56}{desc}".format(num=i, name=k, path=v[0], desc=v[1]))
                 
-                case 8 | 9: # show options to a module TODO:add options
-                    pass
+                case 8 | 9: # show options
+                    if current_module == {}:
+                        print("No module selected")
+                        return
+                    for i in range(len(current_module[list(current_module)[0]][2])):
+                        print("""
+#         NAME                          DESCRIPTION                                         OPTIONAL
+-         ----                          -----------                                         --------""")
+                        print(f"{str(i+1):10}{current_module[list(current_module)[0]][2][i]}")
+
         print()
     
     # commands to use with a module
     def do_use(self, module: str | int):
-        global search_results, search_results_use, modules
-        search_results_use = {}
+        'Select a module'
+        global search_results, current_module, modules
+        current_module = {}
         try:
             module_int = int(module)
             if search_results == {}: return
             if module_int > len(search_results) or module_int <= 0:
                 print("Error, number out of range")
                 return
-            search_results_use[list(search_results)[module_int-1]] = search_results[list(search_results)[module_int-1]]
-            print(search_results_use)
+            current_module[list(search_results)[module_int-1]] = search_results[list(search_results)[module_int-1]]
         except (TypeError, ValueError):
             if module == "":
                 print("Please specify a module to use")
                 return
             f_search_use(module, modules)
-            if search_results_use == {}:
+            if current_module == {}:
                 print("No module found. Try using search %s."%module)
                 return
-            print(search_results_use)
-
-
+        return
 
 if __name__=="__main__":
     command = CLI(completekey='tab') # create instance
